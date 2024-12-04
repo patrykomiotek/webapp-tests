9c8446ad4166b7b80d931be5a987e6d723946918
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-12-04 14:08:51 +0100

    fix: with vite setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { RegistrationFormState } from '@components/RegistrationFormState';
-import { RegistrationFormRefs } from '@components/RegistrationFormRefs';
+// import { RegistrationFormState } from '@components/RegistrationFormState';
+// import { RegistrationFormRefs } from '@components/RegistrationFormRefs';
 import './App.css';
 import { HomePage } from '@pages/Home';
+import { Toast } from '@ui';
 // import { Generator } from "@components/Generator";
 
 function App() {
   return (
     <>
       <HomePage />
+      <Toast />
     </>
   );
 }
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,3 +2,4 @@ export * from './Text';
 export * from './Button';
 // export * from "./Button";
 export * from './Input';
+export * from './Toast';
