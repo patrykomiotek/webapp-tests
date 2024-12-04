3a451ac4018010beba21973cbe48c284b0df701c
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-12-04 10:08:53 +0100

    fix: tsconfig paths for vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import { Text } from "./ui/Text";
+import { Text } from "@ui";
 import "./App.css";
 
 function App() {
