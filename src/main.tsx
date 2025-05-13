0a97fa2c337558fe78bcf95bd85de37637db9426
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-05-13 09:52:27 +0200

    feat: with validation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { z } from 'zod';
+import './index.css';
+import App from './App.tsx';
+
+const envSchema = z.object({
+  VITE_API_BASE_URL: z.string(),
+  TEST_TEST: z.string(),
+});
+
+console.log({ envs: import.meta.env });
+try {
+  const parsedValues = envSchema.parse(import.meta.env);
+} catch (error) {
+  console.log('Env vars error: ', JSON.stringify(error, null, 2));
+  process.exit(1);
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
