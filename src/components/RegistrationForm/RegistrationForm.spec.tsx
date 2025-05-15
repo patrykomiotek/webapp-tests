acf3d4e8f2fe22de738f91279c803aedc4d92451
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-05-15 10:07:13 +0200

    feat: tests screnarios

--- src/components/RegistrationForm/RegistrationForm.spec.tsx
+++ src/components/RegistrationForm/RegistrationForm.spec.tsx
@@ -14,4 +14,12 @@ describe('RegistrationForm component', () => {
     emailField = screen.getByLabelText('E-mail');
     screen.debug(emailField);
   });
+
+  it.todo('should render all fields');
+  it.todo('should render validation messages');
+  it.todo('should validate email address');
+  it.todo('should validate too short password');
+  it.todo('should validate invalid passwords');
+  it.todo('should display success message');
+  it.todo('should clear error message after passing valid data');
 });
